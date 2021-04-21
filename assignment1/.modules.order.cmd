e7689ed2bb5eebdd0b0ffb95ce72d5681f93875f
BrandonNgo721k <brandon.w.ngo@gmail.com>
2021-04-21 15:50:36 -0700

    Delete .modules.order.cmd

--- assignment1/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/brandon/Documents/CMPE283/assignment1/modules.order := {   echo /home/brandon/Documents/CMPE283/assignment1/cmpe283-1.ko; :; } | awk '!x[$$0]++' - > /home/brandon/Documents/CMPE283/assignment1/modules.order

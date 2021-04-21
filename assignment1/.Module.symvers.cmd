b2b1adac46615a47260f3d501441b7ca43d84524
BrandonNgo721k <brandon.w.ngo@gmail.com>
2021-04-21 15:50:31 -0700

    Delete .Module.symvers.cmd

--- assignment1/.Module.symvers.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/brandon/Documents/CMPE283/assignment1/Module.symvers := sed 's/ko$$/o/' /home/brandon/Documents/CMPE283/assignment1/modules.order | scripts/mod/modpost -m -a   -o /home/brandon/Documents/CMPE283/assignment1/Module.symvers -e -i Module.symvers   -T -
